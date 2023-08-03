06992bc412ca1a7024c9ae5e889bfbbf60a3d759
fralonra <fralonra@aliyun.com>
2023-08-03 22:28:56 +0800

    change eframe options

--- src/bin/editor.rs
+++ src/bin/editor.rs
@@ -5,7 +5,11 @@ use ppd_editor::editor;
 fn main() {
     env_logger::init();
 
-    let native_options = eframe::NativeOptions::default();
+    let native_options = eframe::NativeOptions {
+        centered: true,
+        initial_window_size: Some(eframe::epaint::vec2(1200.0, 700.0)),
+        ..Default::default()
+    };
 
     eframe::run_native(
         editor::APP_TITLE,
