f0463a0721c45f34bfe83ef9cebe64d4204baff7
fralonra <fralonra@aliyun.com>
2023-08-06 19:26:56 +0800

    fix window icon

--- src/bin/viewer.rs
+++ src/bin/viewer.rs
@@ -1,30 +1,25 @@
 #![windows_subsystem = "windows"]
 
+use eframe::IconData;
 use ppd_editor::viewer;
 
 fn main() {
     env_logger::init();
 
-    let mut native_options = eframe::NativeOptions {
+    let native_options = eframe::NativeOptions {
         centered: true,
-        initial_window_size: Some(eframe::epaint::vec2(1200.0, 700.0)),
-        ..Default::default()
-    };
-
-    #[cfg(not(target_os = "macos"))]
-    {
-        use eframe::IconData;
-
-        native_options.icon_data = match IconData::try_from_png_bytes(include_bytes!(
-            "../../build/windows/ppd-viewer.ico"
+        icon_data: match IconData::try_from_png_bytes(include_bytes!(
+            "../../build/logo/ppd-viewer.png"
         )) {
             Ok(icon) => Some(icon),
             Err(err) => {
                 log::warn!("Failed to load window icon: {}", err);
                 None
             }
-        };
-    }
+        },
+        initial_window_size: Some(eframe::epaint::vec2(1200.0, 700.0)),
+        ..Default::default()
+    };
 
     eframe::run_native(
         viewer::APP_TITLE,
